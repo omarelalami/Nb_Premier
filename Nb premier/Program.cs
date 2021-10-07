367a10ea8cc951579d708b45dfcd22d6c820d764
omarelalami <omaralami230@gmail.com>
2021-10-07 17:31:18 +0100

    initial commit

---
+++ Nb premier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Nb_premier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int N,cmt=0;
+           
+
+            do
+            {
+                Console.Write("Donner un Nombre Entier: ");
+                N = int.Parse(Console.ReadLine());
+            }
+            while (N < 0);
+            
+        
+                for (int i = 0; i < N / 2; i++)
+                    if (N % 2 == 0)
+                        cmt++;
+                
+                if (cmt > 2)
+                    Console.WriteLine("Ce nombre n'est pas premier");
+                else
+                    Console.WriteLine("Ce nombre est  premier");
+            Console.ReadKey();
+          }
+    }
+}
